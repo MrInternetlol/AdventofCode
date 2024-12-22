b3acb39755e3c2ae7ce45891f3f521159efb3d7e
MrInternetlol <tguss@live.de>
2024-12-22 13:34:35 +0100

    chore: create initial Validate-Reports.ps1 for Day 2 Part 2 puzzle
    Created Validate-Reports.ps1 as a starting point for the second part of the puzzle. This provides a base to build upon without having to rewrite everything from scratch.

---
+++ 2024/Day2/Part02/Validate-Reports.ps1
@@ -0,0 +1,72 @@
+function Get-PuzzleInput {
+    return $(Get-Content -Path ..\Input.txt)
+}
+
+function Validate-Reports
+{
+    param (
+        $Reports
+    )
+
+    $ValidReports = 0
+
+    foreach ($Report in $Reports)
+    {
+        $Levels = $Report -split ' '
+        $isSafe = $true
+
+        if ([int]$Levels[0] -lt [int]$Levels[1])
+        {
+            $isIncreasing = $true
+        }
+        else
+        {
+            $isIncreasing = $false
+        }
+
+        for ($i = 0;$i -lt $Levels.Length;$i++)
+        {
+            if ($i -eq 0)
+            {
+                Continue
+            }
+            if ($isIncreasing)
+            {
+                if ([int]$Levels[$i] -le [int]$Levels[$i - 1])
+                {
+                    $isSafe = $false
+                    break
+                }
+                elseif ([int]$Levels[$i] - [int]$Levels[$i - 1] -gt 3)
+                {
+                    $isSafe = $false
+                    break
+                }
+            }
+            else
+            {
+                if ([int]$Levels[$i] -ge [int]$Levels[$i - 1])
+                {
+                    $isSafe = $false
+                    break
+                }
+                elseif ([int]$Levels[$i-1] - [int]$Levels[$i] -gt 3)
+                {
+                    $isSafe = $false
+                    break
+                }
+
+            }
+        }
+
+        if ($isSafe)
+        {
+            $ValidReports++
+        }
+    }
+
+    return $ValidReports
+}
+
+$PuzzleInput = Get-PuzzleInput
+Validate-Reports -Reports $PuzzleInput
